fcc535fc22df654f48ca417d04b0ac03d75e4b10
ShrutiAgarwal10 <s4shruti10@gmail.com>
2022-05-09 11:01:07 +0530

    added grid projects

---
+++ jQGridInAsp.NetMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using jQGridInAsp.NetMVC.Models;
+
+namespace jQGridInAsp.NetMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Employee/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetData()
+        {
+
+            using (DBModel db = new DBModel())
+            {
+                var employeeList = db.Employees.ToList<Employee>();
+                return Json(new { rows = employeeList }, JsonRequestBehavior.AllowGet);
+            }
+        }
+	}
+}
---
+++ jQGridInAsp.NetMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(jQGridInAsp.NetMVC.Startup))]
+namespace jQGridInAsp.NetMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
